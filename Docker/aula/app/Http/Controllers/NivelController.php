12c7beff326122cafc445b0dbef7b7651f021912
Fellipe Fernandes Nogueira <fellipe.5fernades@gmail.com>
2024-03-27 01:00:21 -0300

    Aparecimento da tela inicial do SIGAC

---
+++ Docker/aula/app/Http/Controllers/NivelController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nivel;
+use App\Repositories\CursoRepository;
+use App\Repositories\NivelRepository;
+use Illuminate\Http\Request;
+
+class NivelController extends Controller
+{
+    protected $repository;
+
+    public function __construct() {
+        $this->repository = new NivelRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = $this->repository->selectAll();
+        return $data;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $obj = new Nivel();
+        $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
+        $this->repository->save($obj);
+        return "<h1>Store - OK!</h1>";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = $this->repository->findById($id);
+
+        if(isset($data)){
+            return $data;
+        }
+
+        return "<h1>Show - ERROR!</h1>";
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $obj = $this->repository->findById($id);
+
+        if(isset($obj)) {
+            $obj->nome = mb_strtoupper($request->nome, 'UTF-8');
+            $this->repository->save($obj);
+            return "<h1>Upate - OK!</h1>";
+        }
+        return "<h1>Upate - Not found Eixo!</h1>";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        if($this->repository->delete($id)) {
+            return "<h1>Delete - OK!</h1>";
+        }
+        
+        return "<h1>Delete - Not found Eixo!</h1>";
+    }
+}
--- Docker/aula/routes/web.php
+++ Docker/aula/routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('site');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
 
 Route::resource('/eixo', 'App\Http\Controllers\EixoController');
 Route::resource('/curso', 'App\Http\Controllers\CursoController');
@@ -24,3 +28,11 @@ Route::resource('/categoria', 'App\Http\Controllers\CategoriaController');
 Route::resource('/usuario', 'App\Http\Controllers\UserController');
 Route::resource('/comprovante', 'App\Http\Controllers\ComprovanteController');
 Route::resource('/declaracao', 'App\Http\Controllers\DeclaracaoController');
+Route::resource('/aluno', 'App\Http\Controllers\AlunoController');
+Route::resource('/nivel', 'App\Http\Controllers\NivelController');
+
+Route::get('/site/register', 'App\Http\Controllers\AlunoController@register')->name('site.register');
+Route::post('/site/success', 'App\Http\Controllers\AlunoController@storeRegister')->name('site.submit');
+
+Route::get('/users/{role}', 'App\Http\Controllers\UserController@getUsersByRole')->name('users.role');
+Route::get('/users/create/{role_id}', 'App\Http\Controllers\UserController@createUsersByRole')->name('users.role.create');
